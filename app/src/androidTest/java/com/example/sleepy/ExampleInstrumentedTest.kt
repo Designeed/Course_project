ba4a02ca74178876f5ea46465b2046bb2e4720c1
Konstkonst55 <77889524+Konstkonst55@users.noreply.github.com>
2022-05-20 23:11:44 +0700

    Написаны тесты для функций расчета времени

--- app/src/androidTest/java/com/example/sleepy/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/sleepy/ExampleInstrumentedTest.kt
@@ -1,21 +1,15 @@
 package com.example.sleepy
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import android.app.AlarmManager
+import android.content.Context
+import android.view.View
 import androidx.test.platform.app.InstrumentationRegistry
+import com.example.sleepy.utils.AlarmUtils
+import com.example.sleepy.utils.TimeUtils
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
+import java.util.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see [Testing documentation](http://d.android.com/tools/testing)
- */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        Assert.assertEquals("com.example.Sleepy", appContext.packageName)
-    }
-}
+
+class ExampleInstrumentedTest
--- app/src/test/java/com/example/sleepy/ExampleUnitTest.kt
+++ app/src/test/java/com/example/sleepy/ExampleUnitTest.kt
@@ -1,16 +1,41 @@
 package com.example.sleepy
 
+import android.app.AlarmManager
+import android.content.Context
+import android.view.View
+import com.example.sleepy.utils.AlarmUtils
+import com.example.sleepy.utils.TimeUtils
 import org.junit.Assert
 import org.junit.Test
+import org.mockito.Mockito.mock
+import java.util.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see [Testing documentation](http://d.android.com/tools/testing)
- */
-class ExampleUnitTest {
+class ExampleUnitTest{
+
     @Test
-    fun addition_isCorrect() {
-        Assert.assertEquals(4, (2 + 2).toLong())
+    fun calcRemainingTimeMinute() {
+        //теститрование функции расчета оставшегося времени
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.DATE, 1)
+        Assert.assertEquals(
+            TimeUtils.calcRemainingTimeMinute(
+                date = Date(calendar.timeInMillis),
+                context = mock(Context::class.java)
+            ),
+            "24null"
+        )
+    }
+
+    @Test
+    fun getFormatTime(){
+        //тестироваие функции форматирования минут в строку
+        val calendar = Calendar.getInstance()
+        calendar.set(2022, 5, 8, 14, 15, 12)
+        Assert.assertEquals(
+            TimeUtils.getFormatTime(
+                minute = calendar.get(Calendar.MINUTE).toLong(),
+                context = mock(Context::class.java)
+            ), "15null"
+        )
     }
 }
